cd7e863faefa54f7d4c9c668a1ab75a716d4f494
Andrey Serikov <AS.Korag@protonmail.com>
2021-07-06 14:14:54 +0300

    add: getWinner method

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -5,6 +5,7 @@ class TicTacToe {
     [null, null, null],
   ]
   #currentPlayerSymbol = 'x'
+  #playerSymbols = ['x', 'o']
   #winner = null
 
   constructor() {}
@@ -17,6 +18,50 @@ class TicTacToe {
     }
   }
 
+  _checkBoard() {
+    let res = null
+
+    //-------Check Rows--------
+    this.#board.forEach(row => {
+      this.#playerSymbols.forEach(symbol => {
+        if (row.every(value => value === symbol)) {
+          res = symbol
+        }
+      })
+    })
+
+    //-------Check Columns-------
+    const col1 = [this.#board[0][0], this.#board[1][0], this.#board[2][0]]
+    const col2 = [this.#board[0][1], this.#board[1][1], this.#board[2][1]]
+    const col3 = [this.#board[0][2], this.#board[1][2], this.#board[2][2]]
+
+    const columns = [col1, col2, col3]
+
+    columns.forEach(col => {
+      this.#playerSymbols.forEach(symbol => {
+        if (col.every(value => value === symbol)) {
+          res = symbol
+        }
+      })
+    })
+
+    //-------Check Diagonals-------
+    const diag1 = [this.#board[0][0], this.#board[1][1], this.#board[2][2]]
+    const diag2 = [this.#board[2][0], this.#board[1][1], this.#board[0][2]]
+
+    const diagonals = [diag1, diag2]
+
+    diagonals.forEach(diag => {
+      this.#playerSymbols.forEach(symbol => {
+        if (diag.every(value => value === symbol)) {
+          res = symbol
+        }
+      })
+    })
+
+    return res
+  }
+
   getCurrentPlayerSymbol() {
     return this.#currentPlayerSymbol
   }
@@ -25,12 +70,15 @@ class TicTacToe {
     if (this.#board[row][col] === null) {
       this.#board[row][col] = this.#currentPlayerSymbol
       this._togglePlayerSymbol()
+      this.#winner = this._checkBoard()
     }
   }
 
   isFinished() {}
 
-  getWinner() {}
+  getWinner() {
+    return this.#winner
+  }
 
   noMoreTurns() {
     return !this.#board.flat().includes(null)
